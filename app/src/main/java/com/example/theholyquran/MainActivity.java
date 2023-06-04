c7da7d45ddf8064756d46439a6a3a6fd389cd1db
S-H-A-B-B-Y <bsef20a507@pucit.edu.pk>
2023-06-04 05:23:56 -0700

    Surah selection commit

--- app/src/main/java/com/example/theholyquran/MainActivity.java
+++ app/src/main/java/com/example/theholyquran/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -29,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Quran=new QDH();
+
+        editText=findViewById(R.id.editTextMain);
+        search=findViewById(R.id.searchMain);
+
         //MainActivity main=new MainActivity();
         ArrayList<String> arrayList=Quran.getSurahNames();
 
@@ -37,6 +42,20 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(arrayAdapter);
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int surahNum=Integer.parseInt(editText.getText().toString());
+
+                if (surahNum>0 && surahNum<=114)
+                {
+                    listView.setSelection(--surahNum);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Surah number is Invalid!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
